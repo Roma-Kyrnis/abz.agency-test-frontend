103f91e5e56e65611cf35f61bb6e2c7fb3314407
Roma-Kyrnis <kirnos007@gmail.com>
2023-12-03 10:45:20 +0200

    fix base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/abz.agency-test-frontend/',
   plugins: [react()],
 });
